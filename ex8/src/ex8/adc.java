1f2cb20292d10835f918a4375cc47f086e5fc575
shanavasbabu <47814621+shanavasbabu@users.noreply.github.com>
2019-02-20 07:17:10 -0800

    Add files via upload

---
+++ ex8/src/ex8/adc.java
@@ -0,0 +1,53 @@
+package ex8;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class adc
+ */
+public class adc extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public adc() {
+    	super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String un=request.getParameter("username");
+		String pw=request.getParameter("password");
+		
+		if(un.equals("admin") && pw.equals("admin"))
+		{
+			response.sendRedirect("success");
+			
+		}
+		else
+		{
+			response.sendRedirect("error");
+			return;
+		}
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
